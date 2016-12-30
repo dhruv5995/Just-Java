05e199fed2de40a10bd76b455752ab22870b87e9
Dhruv Shah <dhruv5995@gmail.com>
2016-12-30 13:01:50 +0530

    Added basic functionality to order button

--- app/src/main/java/com/example/dhruv/justjava/MainActivity.java
+++ app/src/main/java/com/example/dhruv/justjava/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dhruv.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * This app displays an order form to order coffee
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button is clicked
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
 }
