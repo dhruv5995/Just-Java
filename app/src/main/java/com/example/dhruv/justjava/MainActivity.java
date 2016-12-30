e046065f7d22891f0c43a706e1dd59f860754cf6
Dhruv Shah <dhruv5995@gmail.com>
2016-12-30 15:31:20 +0530

    Added increment and decrement button for the number of coffees in the layout

--- app/src/main/java/com/example/dhruv/justjava/MainActivity.java
+++ app/src/main/java/com/example/dhruv/justjava/MainActivity.java
@@ -22,9 +22,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when + button is clicked
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when - button is clicked
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
